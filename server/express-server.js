34e74de334bb1c7b811cfae0466ebaf41228a0e3
jheepark <jhee.park.work@gmail.com>
2017-12-11 18:09:30 +1100

    port

--- server/express-server.js
+++ server/express-server.js
@@ -3,6 +3,6 @@ let app = express();
 
 app.use('/', express.static('./public'));
 
-let server = app.listen(8000, function(){
+let server = app.listen(process.env.PORT || 8000, function(){
   console.log('Server is running...');
 });
